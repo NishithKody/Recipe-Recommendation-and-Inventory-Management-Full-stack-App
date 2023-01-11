f69397633aec74479584870e469dd349d0a1e083
Nishith <nishith0514@gmail.com>
2023-01-11 14:18:41 -0500

    Code push

---
+++ server/config/dev/keys.js
@@ -0,0 +1,11 @@
+//Config file to store important values
+
+export const keys ={
+    "clientID":"945669722753-u7ich7n215bqu92n0kljn9qg419eeqf8.apps.googleusercontent.com",
+    "secretID": "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",
+    "cookieKey": "ihinoiansidboindioabodaoib",
+    "port":8000,
+    "s3AccessKey":"",
+    "s3SecretKey":"",
+    "s3Bucket":""
+}
